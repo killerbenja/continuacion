6c8dfbb4f2304596bc1f385fb922f5312053a543
unknown <Santo Tomas@SCL076PCL8015.alumnos.cst>
2023-08-30 12:36:58 -0400

    subida clase 3

---
+++ app/src/main/java/com/example/example/Vista.java
@@ -0,0 +1,15 @@
+package com.example.example;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Vista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista);
+
+    }
+}
